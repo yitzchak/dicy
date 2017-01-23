ac9a1cf66d14909d6e0b35cef568f72549109b29
Tarn Burton <twburton@gmail.com>
2017-01-23 06:02:52 -0500

    Test for error messages in spec

--- spec/BuilderSpec.js
+++ spec/BuilderSpec.js
@@ -4,18 +4,22 @@ import 'babel-polyfill'
 import path from 'path'
 
 import { Builder } from '../src/main'
+import type { Message } from '../src/types'
 
 describe('Builder', () => {
   let builder: Builder
+  let messages: Array<Message>
 
   beforeEach(async (done) => {
-    const options = { ignoreCache: true, engine: 'pdflatex' }
-    builder = await Builder.create(path.resolve(__dirname, 'fixtures', 'foo.tex'), options, message => console.log(message.text))
+    const options = { ignoreCache: true, engine: 'pdflatex', messageLevel: 'error' }
+    messages = []
+    builder = await Builder.create(path.resolve(__dirname, 'fixtures', 'foo.tex'), options, message => { messages.push(message) })
     done()
   })
 
   it('verifies that Builder run returns expected true', async (done) => {
     expect(await builder.run('build')).toBeTruthy()
+    expect(messages).toEqual([])
     done()
   }, 10000)
 })
--- src/Rules/ReportLogMessages.js
+++ src/Rules/ReportLogMessages.js
@@ -6,7 +6,7 @@ import type { Command, Message, Phase } from '../types'
 
 export default class ReportLogMessages extends Rule {
   static fileTypes: Set<string> = new Set(['ParsedBiberLog', 'ParsedBibTeXLog', 'ParsedLaTeXLog', 'ParsedMakeIndexLog'])
-  static commands: Set<Command> = new Set(['report'])
+  static commands: Set<Command> = new Set(['build', 'report'])
   static phases: Set<Phase> = new Set(['finalize'])
   static alwaysEvaluate: boolean = true
 
