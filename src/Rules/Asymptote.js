24762459ace0a6b43636e11575e121f8e2be42f7
Tarn Burton <twburton@gmail.com>
2017-01-29 14:36:19 -0500

    Add Asymptote

---
+++ src/Rules/Asymptote.js
@@ -0,0 +1,25 @@
+/* @flow */
+
+import path from 'path'
+
+import Rule from '../Rule'
+
+export default class Asymptote extends Rule {
+  static fileTypes: Set<string> = new Set(['Asymptote'])
+
+  constructCommand () {
+    return `asy -offscreen -vv "${path.basename(this.firstParameter.normalizedFilePath)}"`
+  }
+
+  constructProcessOptions (): Object {
+    return {
+      cwd: this.options.outputDirectory
+        ? path.resolve(this.rootPath, this.options.outputDirectory)
+        : this.rootPath
+    }
+  }
+
+  async processOutput (stdout: string, stderr: string): Promise<boolean> {
+    return true
+  }
+}
