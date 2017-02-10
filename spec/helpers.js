3b2cea61656337b6f3fb2c963041a32ad9a2710b
Tarn Burton <twburton@gmail.com>
2017-02-10 07:01:30 -0500

    Update matcher and add pygmentize

--- spec/helpers.js
+++ spec/helpers.js
@@ -48,7 +48,7 @@ function compareFilePaths (x: string, y: string): boolean {
 }
 
 function stringCompare (x: string, y: string): boolean {
-  return x.replace(/[/\\'"]/, '') === y.replace(/[/\\'"]/, '')
+  return x.replace(/[/\\'"^]/g, '') === y.replace(/[/\\'"^]/g, '')
 }
 
 export const customMatchers = {
