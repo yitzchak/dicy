e543cb8757d5cf4e3278315338b26ac99dd6ad13
Tarn Burton <twburton@gmail.com>
2017-06-15 07:17:38 -0400

    Use line wrapping

--- packages/core/src/Rules/ParseKnitrConcordance.js
+++ packages/core/src/Rules/ParseKnitrConcordance.js
@@ -2,7 +2,9 @@
 
 import Rule from '../Rule'
 
-import type { Command, Message } from '../types'
+import type { Command } from '../types'
+
+const WRAPPED_LINE_PATTERN = /%$/
 
 export default class ParseKnitrConcordance extends Rule {
   static fileTypes: Set<string> = new Set(['KnitrConcordance'])
@@ -13,27 +15,32 @@ export default class ParseKnitrConcordance extends Rule {
     const outputFile = await this.getResolvedOutput('$DIR/$BASE-ParsedKnitrConcordance', this.firstParameter)
     if (!outputFile) return false
 
-    let input: ?string
-    let output: ?string
-    let indicies: Array<number> = []
+    const concordances: Array<Object> = []
 
     await this.firstParameter.parse([{
-      names: ['output', 'input'],
-      patterns: [/^\\Sconcordance\{concordance:([^:]*):([^:]*):%$/],
+      names: ['output', 'input', 'indicies'],
+      patterns: [/^\\Sconcordance\{concordance:([^:]*):([^:]*):([^}]*)\}$/],
       evaluate: (reference, groups) => {
-        input = groups.input
-        output = groups.output
-      }
-    }, {
-      names: ['indicies'],
-      patterns: [/^([ 0-9]*)[%}]$/],
-      evaluate: (reference, groups) => {
-        indicies = indicies.concat(groups.indicies.split(/\s+/).map(x => parseInt(x)))
-      }
-    }])
+        const encodedIndicies: Array<number> = groups.indicies.split(/\s+/).map(x => parseInt(x))
+        const indicies: Array<[number, number]> = []
+        let inputLine: number = 1
+        let outputLine: number = 1
 
-    indicies.shift()
-    outputFile.value = { input, output, indicies }
+        for (let i = 1; i < encodedIndicies.length; i += 2) {
+          for (let j = 0; j < encodedIndicies[i]; j++, outputLine++, inputLine += encodedIndicies[i + 1]) {
+            indicies.push([inputLine, inputLine + encodedIndicies[i + 1]])
+          }
+        }
+
+        concordances.push({
+          input: groups.input,
+          output: groups.output,
+          indicies
+        })
+      }
+    }], line => WRAPPED_LINE_PATTERN.test(line))
+
+    outputFile.value = { concordances }
 
     return true
   }
