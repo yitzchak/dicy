ffd78e5fc3ab4d1654288a8efc9ae27544a28581
Tarn Burton <twburton@gmail.com>
2017-06-19 12:52:10 -0400

    Add more comments to File

--- packages/core/src/File.js
+++ packages/core/src/File.js
@@ -14,20 +14,45 @@ import type { FileType, Parser, Reference } from './types'
 export default class File {
   static fileTypes: Map<string, FileType>
 
+  // The complete and real file path in the file system.
   realFilePath: string
+  // The file path relative to the project root.
   filePath: string
+  // The main type of the file, i.e. LaTeX, BibTeX, ...
   type: string
+  // An optional sub type. Usually the document class for LaTeX documents.
   subType: ?string
+  // Last update time of the file.
   timeStamp: Date
+  // If it is a virtual or a physical file. Virtual files are usually in-memory
+  // copies of parsed files such as log files.
   virtual: boolean = false
+  // A hash of the file contents. Used to verify that file has actually changed
+  // when the timestamp changes
   hash: string
+  // Rules that have the file as an input.
   rules: Set<Rule> = new Set()
+  // Job names currently associated with the file.
   jobNames: Set<string> = new Set()
+  // Has the file been analyzed in the current cycle?
   analyzed: boolean = false
+  // Has the file been updated in the current cycle?
   _hasBeenUpdated: boolean = false
+  // Has the file been changed during the current run?
   hasBeenUpdatedCache: boolean = false
+  // The value of the virtual file.
   _value: ?any
 
+  /**
+   * Construct a new File. Because creating a file required asynchronous file
+   * system access this method is used only to initialize the File instance. Used
+   * the `create` method to actual create an instance.
+   * @param  {string} realFilePath The actual file system path.
+   * @param  {string} filePath     The file path relative to the project root.
+   * @param  {Date}   timeStamp    The time of the last update provided by cache.
+   * @param  {string} hash         The last content hash provided by cache.
+   * @param  {any}    value        The value of the virtual file.
+   */
   constructor (realFilePath: string, filePath: string, timeStamp: ?Date, hash: ?string, value: ?any) {
     this.realFilePath = realFilePath
     this.filePath = filePath
@@ -36,29 +61,60 @@ export default class File {
     if (value) this._value = value
   }
 
+  /**
+   * Create a new File.
+   * @param  {string} realFilePath The actual file system path.
+   * @param  {string} filePath     The file path relative to the project root.
+   * @param  {Date}   timeStamp    The time of the last update provided by cache.
+   * @param  {string} hash         The last content hash provided by cache.
+   * @param  {any}    value        The value of the virtual file.
+   * @return {File}                The File instance.
+   */
   static async create (realFilePath: string, filePath: string, timeStamp: ?Date, hash: ?string, value: ?any): Promise<?File> {
     const file: File = new File(realFilePath, filePath, timeStamp, hash, value)
 
     await file.findType()
+    // If the file type is not a virtual file type and there is no physical file
+    // then just quit.
     if (!file.virtual && !await File.canRead(realFilePath)) return
+    // Check for an update to file in case it has changed since the cache was
+    // finalized.
     file.hasBeenUpdated = await file.updateTimeStamp() && await file.updateHash()
 
     return file
   }
 
+  /**
+   * Parse the file using a list of Parsers.
+   * @param  {Array<Parser>}      parsers   List of parsers to apply.
+   * @param  {string => boolean}  isWrapped A function to test for line wrapping.
+   */
   parse (parsers: Array<Parser>, isWrapped: string => boolean = line => false): Promise<void> {
     return new Promise((resolve, reject) => {
+      // The maximum number of lines that we need to maintain in a buffer to
+      // satisfy all the parsers.
       const bufferSize = parsers.reduce((current, parser) => Math.max(current, parser.patterns.length), 0)
-      type foo = { text: string, count: number }
-      let lines: Array<foo> = []
+      // A type representing an unwrapped line where `count` is the number of
+      // wrapped lines in the file that each unwrapped line represents.
+      type Line = { text: string, count: number }
+      // The buffer of unwrapped lines.
+      let lines: Array<Line> = []
       let lineNumber = 1
+      // A function to check form matches in all the parsers.
       const checkForMatches = (finalCheck: boolean = false) => {
         while (lines.length > 0) {
+          // If this is not the final check then do not check if there is not
+          // enough lines in the buffer to check all parsers.
           if (!finalCheck && lines.length < bufferSize) break
-          let matched = false
+
+          let matched: boolean = false
+
           for (const parser: Parser of parsers) {
+            // If there is not enough lines to check this parser then skip it.
             if (parser.patterns.length > lines.length) continue
+
             const matches = parser.patterns.map((pattern, index) => lines[index].text.match(pattern))
+
             matched = matches.every(match => match)
             if (matched) {
               // $FlowIgnore
@@ -79,6 +135,7 @@ export default class File {
             }
           }
           if (!matched) {
+            // No match so pop the leading line from the queue.
             lineNumber += lines[0].count
             lines.shift()
           }
@@ -98,7 +155,7 @@ export default class File {
         checkForMatches(true)
         resolve()
       } else {
-        let current: foo = { text: '', count: 0 }
+        let current: Line = { text: '', count: 0 }
         const rl = readline.createInterface({
           input: fs.createReadStream(this.realFilePath, { encoding: 'utf-8' })
         })
@@ -121,12 +178,21 @@ export default class File {
     })
   }
 
+  /**
+   * Getter for `hasBeenUpdated`
+   * @return {Boolean}  Update status.
+   */
   get hasBeenUpdated (): boolean {
     return this._hasBeenUpdated
   }
 
+  /**
+   * Setter for `hasBeenUpdated`
+   * @param {Boolean}  value  Update status.
+   */
   set hasBeenUpdated (value: boolean) {
     this._hasBeenUpdated = value
+    // Cache the update flag in case it gets reset in the cycle.
     this.hasBeenUpdatedCache = value || this.hasBeenUpdatedCache
   }
 
@@ -196,13 +262,18 @@ export default class File {
     }
 
     return new Promise((resolve, reject) => {
+      // If file name does not match required pattern then just quit.
       if (fileType.fileName && !fileType.fileName.test(this.realFilePath)) {
         return resolve(false)
       }
 
+      // Does the file type descriptor require specific file contents pattern?
       if (fileType.contents) {
+        // Make sure the file is readable.
         File.canRead(this.realFilePath).then(canRead => {
           if (canRead) {
+            // Create a line reading interface and interate through the lines
+            // looking for a content match.
             const rl = readline.createInterface({
               input: fs.createReadStream(this.realFilePath)
             })
@@ -212,6 +283,7 @@ export default class File {
               .on('line', line => {
                 const [value, subType] = line.match(fileType.contents) || []
                 if (value) {
+                  // We have a match so set the type and sub type.
                   match = true
                   this.type = name
                   this.subType = subType
@@ -224,6 +296,7 @@ export default class File {
           } else resolve(false)
         })
       } else {
+        // No specific contents required so we have a successful match!
         this.type = name
         resolve(true)
       }
