8e74a77e6ef5635758420f83b55e502637e90487
Tarn Burton <twburton@gmail.com>
2017-06-29 08:14:07 -0400

    Add test for assignOptions

--- packages/core/spec/Rules/ApplyOptionsSpec.js
+++ packages/core/spec/Rules/ApplyOptionsSpec.js
@@ -13,15 +13,50 @@ describe('ApplyOptions', () => {
   let applyOptions: ApplyOptions
 
   beforeEach(async (done) => {
-    state = await State.create(path.resolve(fixturesPath, 'file.tex'))
+    state = await State.create(path.resolve(fixturesPath, 'file.tex'), [{
+      name: 'a',
+      type: 'boolean',
+      description: 'A',
+      commands: []
+    }, {
+      name: 'b',
+      type: 'string',
+      description: 'B',
+      commands: []
+    }, {
+      name: 'c',
+      type: 'number',
+      description: 'C',
+      commands: []
+    }])
     state.env.HOME = fixturesPath
     applyOptions = new ApplyOptions(state, 'load', 'execute')
     done()
   })
 
-  it('', async (done) => {
-    const f: File = await applyOptions.getResolvedFile('$NAME.yaml-ParsedYAML')
-    const g: File = await applyOptions.getResolvedFile('$BASE-ParsedLaTeXMagic')
+  it('verifies that high priority configuration overrides low priority configuration.', async (done) => {
+    const yaml: ?File = await applyOptions.getResolvedFile('$NAME.yaml-ParsedYAML')
+    const magic: ?File = await applyOptions.getResolvedFile('$BASE-ParsedLaTeXMagic')
+
+    expect(yaml).toBeDefined()
+    expect(magic).toBeDefined()
+
+    if (yaml && magic) {
+      yaml.value = {
+        a: true,
+        b: 'foo'
+      }
+      magic.value = {
+        a: false,
+        c: 743
+      }
+
+      await applyOptions.assignOptions()
+
+      expect(state.options.a).toBe(false)
+      expect(state.options.b).toBe('foo')
+      expect(state.options.c).toBe(743)
+    }
 
     done()
   })
