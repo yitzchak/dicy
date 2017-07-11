082516ea2d64221b7d1e7893ddc01a16341f3fcc
Tarn Burton <twburton@gmail.com>
2017-07-10 20:58:09 -0400

    Add literate Agda

--- packages/core/src/Rules/LhsToTeX.js
+++ packages/core/src/Rules/LhsToTeX.js
@@ -5,12 +5,23 @@ import Rule from '../Rule'
 import type { CommandOptions } from '../types'
 
 export default class LhsToTeX extends Rule {
-  static parameterTypes: Array<Set<string>> = [new Set(['LiterateHaskell'])]
-  static description: string = 'Runs lhs2TeX on lhs files.'
+  static parameterTypes: Array<Set<string>> = [new Set([
+    'LiterateHaskell',
+    'LiterateAgda'
+  ])]
+  static description: string = 'Runs lhs2TeX on lhs or lagda files.'
 
   constructCommand (): CommandOptions {
+    const args = ['lhs2TeX']
+
+    if (this.firstParameter.type === 'LiterateAgda') {
+      args.push('--agda')
+    }
+
+    args.push('-o', '$DIR_0/$NAME_0.tex', '$DIR_0/$BASE_0')
+
     return {
-      args: ['lhs2TeX', '-o', '$DIR_0/$NAME_0.tex', '$DIR_0/$BASE_0'],
+      args,
       cd: '$ROOTDIR',
       severity: 'error',
       outputs: ['$DIR_0/$NAME_0.tex']
