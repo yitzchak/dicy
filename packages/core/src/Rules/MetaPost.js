334f38e569d6722d6dca4ba0841d5e9801b9e12a
Tarn Burton <twburton@gmail.com>
2017-07-14 12:15:47 -0400

    Improve MetaPost file detection

--- packages/core/src/Rules/MetaPost.js
+++ packages/core/src/Rules/MetaPost.js
@@ -1,22 +1,13 @@
 /* @flow */
 
-import path from 'path'
-
 import File from '../File'
 import Rule from '../Rule'
-import State from '../State'
 
-import type { Action, Command, CommandOptions, Phase } from '../types'
+import type { Action, CommandOptions } from '../types'
 
 export default class MetaPost extends Rule {
   static parameterTypes: Array<Set<string>> = [new Set(['MetaPost'])]
   static description: string = 'Runs MetaPost on produced MetaPost files.'
-
-  static async appliesToFile (state: State, command: Command, phase: Phase, jobName: ?string, file: File): Promise<boolean> {
-    return await super.appliesToFile(state, command, phase, jobName, file) &&
-      // This is an awful hack. Do we need to look for end/bye?
-      !path.isAbsolute(file.filePath)
-  }
 
   async initialize () {
     await this.getResolvedInput('$DIR_0/$NAME_0.fls-ParsedFileListing')
