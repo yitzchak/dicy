bc6416db9ca07f1c07cb2f130d698f4d9e58c5ae
Tarn Burton <twburton@gmail.com>
2017-08-16 10:50:26 -0400

    Add PythonTeX support

---
+++ packages/core/spec/Rules/PythonTeX_spec.js
@@ -0,0 +1,33 @@
+/* @flow */
+
+import 'babel-polyfill'
+
+import PythonTeX from '../../src/Rules/PythonTeX'
+import { initializeRule } from '../helpers'
+
+async function initialize (options: Object = {}) {
+  return initializeRule({
+    RuleClass: PythonTeX,
+    parameters: [{
+      filePath: 'PythonTeX.pytxcode'
+    }],
+    options
+  })
+}
+
+describe('PythonTeX', () => {
+  describe('constructCommand', () => {
+    it('returns correct arguments and command options for PythonTeX file.', async (done) => {
+      const { rule } = await initialize()
+
+      expect(rule.constructCommand()).toEqual({
+        args: ['pythontex', '$NAME_0'],
+        cd: '$ROOTDIR_0',
+        severity: 'error',
+        globbedOutputs: ['$DIR_0/pythontex-files-$NAME_0/*']
+      })
+
+      done()
+    })
+  })
+})
