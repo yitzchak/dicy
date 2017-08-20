6b05b0b972d7791ef7b718e25971aca39c65b067
Tarn Burton <twburton@gmail.com>
2017-08-20 18:49:59 -0400

    Add comments

--- packages/core/src/Rules/Pweave.js
+++ packages/core/src/Rules/Pweave.js
@@ -14,6 +14,7 @@ export default class Pweave extends Rule {
     const cacheDirectory = this.options.pweaveCacheDirectory
     const figureDirectory = this.options.pweaveFigureDirectory
     const outputPath = this.options.pweaveOutputPath
+    // Always add output format and output path.
     const args = [
       'pweave',
       `--format=${this.options.pweaveOutputFormat}`,
@@ -21,24 +22,31 @@ export default class Pweave extends Rule {
     ]
     let figureFormat = this.options.pweaveFigureFormat
 
+    // If the cache directory is not the default then set it.
     if (cacheDirectory !== 'cache') {
       args.push(`--cache-directory={{${cacheDirectory}}}`)
     }
 
+    // If the figure directory is not the default then set it.
     if (figureDirectory !== 'figures') {
       args.push(`--figure-directory={{${figureDirectory}}}`)
     }
 
+    // Add the documentation mode option if it is set.
     if (this.options.pweaveDocumentationMode) {
       args.push('--documentation-mode')
     }
 
+    // If the output format is pdf the set the output format to pdf unless
+    // the current LaTeX engine cannot produce pdf files. In that case use eps
+    // files.
     if (figureFormat === 'auto') {
       figureFormat = (this.options.outputFormat === 'pdf' && PDF_CAPABLE_LATEX_PATTERN.test(this.options.engine))
         ? 'pdf'
         : 'eps'
     }
 
+    // Only set the option if it is not pdf
     if (figureFormat !== 'pdf') {
       args.push(`--figure-format=${figureFormat}`)
     }
