2e52776fcbc0129a78a02adb9d8d230868319596
Tarn Burton <twburton@gmail.com>
2017-08-24 06:59:18 -0400

    Use appliesToPhase to prevent running

--- packages/core/src/Rules/CreateOutputTree.js
+++ packages/core/src/Rules/CreateOutputTree.js
@@ -4,23 +4,29 @@ import path from 'path'
 
 import File from '../File'
 import Rule from '../Rule'
+import State from '../State'
 
-import type { Phase } from '../types'
+import type { Command, OptionsInterface, Phase } from '../types'
 
 export default class CreateOutputTree extends Rule {
   static phases: Set<Phase> = new Set(['initialize'])
   static alwaysEvaluate: boolean = true
   static description: string = 'Create directory tree for aux files when `outputDirectory` is set.'
 
+  static async appliesToPhase (state: State, command: Command, phase: Phase, options: OptionsInterface): Promise<boolean> {
+    return await Rule.appliesToPhase(state, command, phase, options) &&
+      !!options.outputDirectory
+  }
+
   async run () {
-    if (this.options.outputDirectory) {
-      const directories = await this.globPath('**/*', {
-        types: 'directories',
-        ignorePattern: `${this.options.outputDirectory}/**`
-      })
-      directories.unshift('.')
-      await Promise.all(directories.map(directory => File.ensureDir(path.resolve(this.rootPath, this.options.outputDirectory || '.', directory))))
-    }
+    const directories = await this.globPath('**/*', {
+      types: 'directories',
+      ignorePattern: `${this.options.outputDirectory || '.'}/**`
+    })
+    directories.unshift('.')
+
+    await Promise.all(directories.map(directory => File.ensureDir(path.resolve(this.rootPath, this.options.outputDirectory || '.', directory))))
+
     return true
   }
 }
