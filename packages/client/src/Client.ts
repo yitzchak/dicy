eb513ded6c5f329ae5c9613befb5e13c91c20461
Tarn Burton <twburton@gmail.com>
2017-11-08 15:35:09 -0500

    Add RPC client

---
+++ packages/client/src/Client.ts
@@ -0,0 +1,167 @@
+import * as cp from 'child_process'
+import * as rpc from 'vscode-jsonrpc'
+
+export type Command = 'build' | 'clean' | 'graph' | 'load' | 'log' | 'save' | 'scrub'
+
+export interface LineRange {
+  start: number
+  end: number
+}
+
+export interface Reference {
+  file: string
+  range?: LineRange
+}
+
+export declare type Severity = 'info' | 'warning' | 'error'
+
+export interface Message {
+  severity: Severity
+  text: string
+  name?: string
+  category?: string
+  source?: Reference
+  log?: Reference
+}
+
+export interface LogEvent extends Message {
+  type: 'log'
+}
+
+export interface ActionEvent {
+  type: 'action'
+  rule: string
+  action: string
+  triggers: string[]
+}
+
+export interface CommandEvent {
+  type: 'command'
+  rule: string
+  command: string
+}
+
+export interface FileEvent {
+  type: 'fileChanged' | 'fileAdded' | 'fileDeleted' | 'fileRemoved'
+  file: string
+  virtual?: boolean
+}
+
+export interface InputOutputEvent {
+  type: 'inputAdded' | 'outputAdded'
+  rule: string
+  file: string
+  virtual?: boolean
+}
+
+export type Event = LogEvent | ActionEvent | CommandEvent | FileEvent | InputOutputEvent
+
+export interface Option {
+  name: string
+  type: 'string' | 'strings' | 'number' | 'boolean' | 'variable'
+  defaultValue?: any
+  description: string
+  values?: any[]
+  aliases?: string[]
+  commands?: string[]
+  noInvalidate?: boolean
+}
+
+export class Client {
+  private server: cp.ChildProcess
+  private connection: any
+
+  private actionNotification = new rpc.NotificationType2<string, ActionEvent, void>('action')
+  private commandNotification = new rpc.NotificationType2<string, CommandEvent, void>('command')
+  private deleteRequest = new rpc.RequestType1<string, boolean, void, void>('delete')
+  private exitNotification = new rpc.NotificationType0<void>('exit')
+  private fileAddedNotification = new rpc.NotificationType2<string, FileEvent, void>('fileAdded')
+  private fileChangedNotification = new rpc.NotificationType2<string, FileEvent, void>('fileChanged')
+  private fileDeletedNotification = new rpc.NotificationType2<string, FileEvent, void>('fileAdded')
+  private fileRemovedNotification = new rpc.NotificationType2<string, FileEvent, void>('fileRemoved')
+  private getTargetPathsRequest = new rpc.RequestType1<string, string[], void, void>('getTargetPaths')
+  private inputAddedNotification = new rpc.NotificationType2<string, InputOutputEvent, void>('inputAdded')
+  private killRequest = new rpc.RequestType1<string, boolean, void, void>('kill')
+  private logNotification = new rpc.NotificationType2<string, LogEvent, void>('log')
+  private outputAddedNotification = new rpc.NotificationType2<string, InputOutputEvent, void>('outputAdded')
+  private runRequest = new rpc.RequestType2<string, Command[], boolean, void, void>('run')
+  private setInstanceOptionsRequest = new rpc.RequestType2<string, object, void, void, void>('setInstanceOptions')
+  private updateOptionsRequest = new rpc.RequestType3<string, object, boolean | undefined, object, void, void>('updateOptions')
+
+  async start (): Promise<void> {
+    const serverPath = require.resolve('@dicy/server')
+
+    this.server = cp.fork(serverPath, ['--node-ipc'])
+
+    const input = new rpc.IPCMessageReader(this.server)
+    const output = new rpc.IPCMessageWriter(this.server)
+
+    this.connection = rpc.createMessageConnection(input, output)
+
+    this.connection.listen()
+  }
+
+  exit (): void {
+    this.connection.sendNotification(this.exitNotification)
+  }
+
+  getTargetPaths (filePath: string): Promise<string[]> {
+    return this.connection.sendRequest(this.getTargetPathsRequest, filePath)
+  }
+
+  delete (filePath: string): Promise<void> {
+    return this.connection.sendRequest(this.deleteRequest, filePath)
+  }
+
+  kill (filePath: string): Promise<void> {
+    return this.connection.sendRequest(this.killRequest, filePath)
+  }
+
+  setInstanceOptions (filePath: string, options: object): Promise<boolean> {
+    return this.connection.sendRequest(this.setInstanceOptionsRequest, filePath, options)
+  }
+
+  run (filePath: string, commands: Command[]): Promise<boolean> {
+    return this.connection.sendRequest(this.runRequest, filePath, commands)
+  }
+
+  updateOptions (filePath: string, options: object, user?: boolean): Promise<object> {
+    return this.connection.sendRequest(this.updateOptionsRequest, filePath, options, user)
+  }
+
+  onAction (handler: (filePath: string, event: ActionEvent) => void): void {
+    this.connection.onNotification(this.actionNotification, handler)
+  }
+
+  onCommand (handler: (filePath: string, event: CommandEvent) => void): void {
+    this.connection.onNotification(this.commandNotification, handler)
+  }
+
+  onFileAdded (handler: (filePath: string, event: FileEvent) => void): void {
+    this.connection.onNotification(this.fileAddedNotification, handler)
+  }
+
+  onFileChanged (handler: (filePath: string, event: FileEvent) => void): void {
+    this.connection.onNotification(this.fileChangedNotification, handler)
+  }
+
+  onFileDeleted (handler: (filePath: string, event: CommandEvent) => void): void {
+    this.connection.onNotification(this.fileDeletedNotification, handler)
+  }
+
+  onFileRemoved (handler: (filePath: string, event: FileEvent) => void): void {
+    this.connection.onNotification(this.fileRemovedNotification, handler)
+  }
+
+  onInputAdded (handler: (filePath: string, event: InputOutputEvent) => void): void {
+    this.connection.onNotification(this.inputAddedNotification, handler)
+  }
+
+  onLog (handler: (filePath: string, event: LogEvent) => void): void {
+    this.connection.onNotification(this.logNotification, handler)
+  }
+
+  onOutputAdded (handler: (filePath: string, event: InputOutputEvent) => void): void {
+    this.connection.onNotification(this.outputAddedNotification, handler)
+  }
+}
---
+++ packages/client/types/main.d.ts
@@ -0,0 +1,86 @@
+export declare type Command = 'build' | 'clean' | 'graph' | 'load' | 'log' | 'save' | 'scrub';
+
+export interface LineRange {
+  start: number;
+  end: number;
+}
+
+export interface Reference {
+  file: string;
+  range?: LineRange;
+}
+
+export declare type Severity = 'info' | 'warning' | 'error';
+
+export interface Message {
+  severity: Severity;
+  text: string;
+  name?: string;
+  category?: string;
+  source?: Reference;
+  log?: Reference;
+}
+
+export interface LogEvent extends Message {
+  type: 'log';
+}
+
+export interface ActionEvent {
+  type: 'action';
+  rule: string;
+  action: string;
+  triggers: string[];
+}
+
+export interface CommandEvent {
+  type: 'command';
+  rule: string;
+  command: string;
+}
+
+export interface FileEvent {
+  type: 'fileChanged' | 'fileAdded' | 'fileDeleted' | 'fileRemoved';
+  file: string;
+  virtual?: boolean;
+}
+
+export interface InputOutputEvent {
+  type: 'inputAdded' | 'outputAdded';
+  rule: string;
+  file: string;
+  virtual?: boolean;
+}
+
+export declare type Event = LogEvent | ActionEvent | CommandEvent | FileEvent | InputOutputEvent;
+
+export interface Option {
+  name: string;
+  type: 'string' | 'strings' | 'number' | 'boolean' | 'variable';
+  defaultValue?: any;
+  description: string;
+  values?: any[];
+  aliases?: string[];
+  commands?: string[];
+  noInvalidate?: boolean;
+}
+
+export interface Client {
+  start (argv: any): Promise<void>;
+  exit (): void;
+  getTargetPaths (filePath: string): Promise<string[]>;
+  delete (filePath: string): Promise<void>;
+  kill (filePath: string): Promise<void>;
+  setInstanceOptions (filePath: string, options: object): Promise<boolean>;
+  run (filePath: string, commands: Command[]): Promise<boolean>;
+  updateOptions (filePath: string, options: object, user?: boolean): Promise<object>;
+
+  onAction (handler: (filePath: string, event: ActionEvent) => void): void;
+  onCommand (handler: (filePath: string, event: CommandEvent) => void): void;
+  onFileAdded (handler: (filePath: string, event: FileEvent) => void): void;
+  onFileChanged (handler: (filePath: string, event: FileEvent) => void): void;
+  onFileDeleted (handler: (filePath: string, event: CommandEvent) => void): void;
+  onFileRemoved (handler: (filePath: string, event: FileEvent) => void): void;
+  onInputAdded (handler: (filePath: string, event: InputOutputEvent) => void): void;
+  onLog (handler: (filePath: string, event: LogEvent) => void): void;
+  onOutputAdded (handler: (filePath: string, event: InputOutputEvent) => void): void;
+}
