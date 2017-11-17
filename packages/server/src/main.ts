00409e6c75188a6e76e8a7a5d39fcd03af718da2
Tarn Burton <twburton@gmail.com>
2017-11-17 06:57:01 -0500

    Fix stdio reader/writer

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -39,7 +39,7 @@ const argv = yargs
 let transport: [rpc.MessageReader, rpc.MessageWriter] | undefined
 
 if (argv.stdio) {
-  transport = [new rpc.StreamMessageReader(process.stdout), new rpc.StreamMessageWriter(process.stdin)]
+  transport = [new rpc.StreamMessageReader(process.stdin), new rpc.StreamMessageWriter(process.stdout)]
 } else if (argv.nodeIpc) {
   transport = [new rpc.IPCMessageReader(process), new rpc.IPCMessageWriter(process)]
 } else if (argv.port) {
