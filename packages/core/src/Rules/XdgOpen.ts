177d7d957a9d7f46d39a6108d1d8f67f540fba4a
Tarn Burton <twburton@gmail.com>
2018-01-23 12:57:21 -0500

    Add parameter types

--- packages/core/src/Rules/XdgOpen.ts
+++ packages/core/src/Rules/XdgOpen.ts
@@ -7,12 +7,16 @@ import { CommandOptions, Group, Phase } from '../types'
 
 export default class XdgOpen extends Rule {
   static commands: Set<Command> = new Set<Command>(['open'])
-  static parameterTypes: Set<string>[] = [new Set(['*'])]
+  static parameterTypes: Set<string>[] = [new Set([
+    'DeviceIndependentFile', 'PortableDocumentFormat', 'PostScript',
+    'ScalableVectorGraphics'
+  ])]
   static alwaysEvaluate: boolean = true
   static description: string = 'Open targets using xdg-open.'
 
   static async isApplicable (consumer: StateConsumer, command: Command, phase: Phase, parameters: File[] = []): Promise<boolean> {
-    return parameters.every(file => !file.virtual && consumer.isOutputTarget(file))
+    return process.platform === 'linux' &&
+      parameters.every(file => !file.virtual && consumer.isOutputTarget(file))
   }
 
   get group (): Group | undefined {
