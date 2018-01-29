febe4c6b5c453cf6da425280c85bb519b9f07a0a
Tarn Burton <twburton@gmail.com>
2018-01-29 06:18:19 -0500

    Add ShellOpen

---
+++ packages/core/src/Rules/ShellOpen.ts
@@ -0,0 +1,33 @@
+import { Command } from '@dicy/types'
+
+import File from '../File'
+import Rule from '../Rule'
+import StateConsumer from '../StateConsumer'
+import { CommandOptions, Group, Phase } from '../types'
+
+export default class ShellOpen extends Rule {
+  static commands: Set<Command> = new Set<Command>(['open'])
+  static parameterTypes: Set<string>[] = [new Set([
+    'DeviceIndependentFile', 'PortableDocumentFormat', 'PostScript',
+    'ScalableVectorGraphics'
+  ])]
+  static alwaysEvaluate: boolean = true
+  static description: string = 'Open targets using Windows shell open.'
+
+  static async isApplicable (consumer: StateConsumer, command: Command, phase: Phase, parameters: File[] = []): Promise<boolean> {
+    return process.platform === 'win32' &&
+      parameters.every(file => !file.virtual && consumer.isOutputTarget(file))
+  }
+
+  get group (): Group | undefined {
+    return 'opener'
+  }
+
+  constructCommand (): CommandOptions {
+    return {
+      command: ['{{$FILEPATH_0}}'],
+      cd: '$ROOTDIR',
+      severity: 'warning'
+    }
+  }
+}
