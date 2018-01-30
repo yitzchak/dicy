ad9db6f6caa8a09f004ddc49816e3ef78c651289
Tarn Burton <twburton@gmail.com>
2018-01-30 08:07:20 -0500

    Use displayline instead of own script

--- packages/core/src/Rules/Skim.ts
+++ packages/core/src/Rules/Skim.ts
@@ -6,6 +6,8 @@ import Rule from '../Rule'
 import StateConsumer from '../StateConsumer'
 import { CommandOptions, Group, Phase } from '../types'
 
+const DISPLAYLINE_PATH: string = '/Applications/Skim.app/Contents/SharedSupport/displayline'
+
 export default class Skim extends Rule {
   static commands: Set<Command> = new Set<Command>(['open'])
   static parameterTypes: Set<string>[] = [new Set([
@@ -15,52 +17,45 @@ export default class Skim extends Rule {
   static description: string = 'Open targets using Skim.'
 
   static async isApplicable (consumer: StateConsumer, command: Command, phase: Phase, parameters: File[] = []): Promise<boolean> {
-    if (process.platform === 'darwin' &&
-      parameters.every(file => !file.virtual && consumer.isOutputTarget(file))) {
-      try {
-        const { stdout } = await consumer.executeCommand(this.constructScript('get version of application "Skim"'))
-        return !!stdout
-      } catch (error) {
-        return false
-      }
+    if (process.platform !== 'darwin' ||
+      parameters.some(file => file.virtual || !consumer.isOutputTarget(file))) {
+      return false
     }
 
-    return false
+    try {
+      await consumer.executeCommand({
+        command: [DISPLAYLINE_PATH, '-h'],
+        cd: '$ROOTDIR',
+        severity: 'info'
+      })
+    } catch (error) {
+      return false
+    }
+
+    return true
   }
 
   get group (): Group | undefined {
     return 'opener'
   }
 
-  static constructScript (script: string): CommandOptions {
+  constructCommand (): CommandOptions {
+    const command: string[] = [DISPLAYLINE_PATH, '-b']
+
+    if (this.options.openInBackground) {
+      command.push('-g')
+    }
+
+    command.push('{{$FILEPATH_0}}', this.options.sourceLine.toString())
+
+    if (this.options.sourcePath) {
+      command.push(path.resolve(this.rootPath, this.options.sourcePath))
+    }
+
     return {
-      command: ['osascript', '-e', script],
+      command,
       cd: '$ROOTDIR',
       severity: 'warning'
     }
-  }
-
-  constructCommand (): CommandOptions {
-    const forwardSync: string = this.options.sourcePath
-      ? `
-  set theSource to POSIX file "${this.options.sourcePath}"
-  set theLine to "${this.options.sourceLine.toString()}" as integer
-  tell front document to go to TeX line theLine from theSource`
-      : ''
-
-    const script: string = `
-set theFile to POSIX file "${this.firstParameter.realFilePath}"
-set thePath to POSIX path of (theFile as alias)
-tell application "Skim"
-  if ${!this.options.openInBackground} then activate
-  try
-    set theDocs to get documents whose path is thePath
-    if (count of theDocs) > 0 then revert theDocs
-  end try
-  open theFile
-  ${forwardSync}
-end tell`
-
-  return Skim.constructScript(script)
   }
 }
